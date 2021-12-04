e40e450fd960d414a5482c1e870ce520370456bf
Leonid Semeniuk <leo.sam@ukr.net>
2021-12-04 13:17:02 +0200

    [FE]:ADD: Get files in client, main page, filelist, file components

--- client/src/reducers/fileReducer.js
+++ client/src/reducers/fileReducer.js
@@ -1,12 +1,21 @@
+const SET_FILES = 'SET_FILES';
+const SET_CURRENT_DIR = 'SET_CURRENT_DIR';
 
 const defaultState = {
-
-}
+  files: [],
+  SET_CURRENT_DIR: null,
+};
 
 export default function fileReducer(state = defaultState, action) {
-    switch (action.type) {
-
-        default:
-            return state
-    }
+  switch (action.type) {
+    case SET_FILES:
+      return { ...state, files: action.payload };
+    case SET_CURRENT_DIR:
+      return { ...state, currentDir: action.payload };
+    default:
+      return state;
+  }
 }
+
+export const setFiles = fiels => ({ type: SET_FILES, payload: fiels });
+export const setCurrentDir = currentDir => ({ type: SET_CURRENT_DIR, payload: currentDir });
