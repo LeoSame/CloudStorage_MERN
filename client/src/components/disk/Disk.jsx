ed6f56ccc8635fee0e37917407aeaf32267414fe
Leonid Semeniuk <leo.sam@ukr.net>
2021-12-04 22:46:33 +0200

    [FE]:ADD: Direction navigation

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { createDir, getFiles } from '../../actions/file';
+import { getFiles } from '../../actions/file';
 import FileList from './file.list/FileList';
 import './disk.css';
 import Popup from './Popup';
-import { setPopupDisplay } from '../../reducers/fileReducer';
+import { setCurrentDir, setPopupDisplay } from '../../reducers/fileReducer';
 
 const Disk = () => {
   const dispatch = useDispatch();
   const currentDir = useSelector(state => state.files.currentDir);
+  const dirStack = useSelector(state => state.files.dirStack);
 
   useEffect(() => {
     dispatch(getFiles(currentDir));
@@ -18,10 +19,19 @@ const Disk = () => {
     dispatch(setPopupDisplay('flex'));
   }
 
+  function backClickHandler() {
+    const backDirId = dirStack.pop();
+    dispatch(setCurrentDir(backDirId));
+  }
+
   return (
     <div className='disk'>
       <div className='disk__btns'>
-        <button className='disk__back'>Назад</button>
+        {currentDir && (
+          <button className='disk__back' onClick={() => backClickHandler()}>
+            Назад
+          </button>
+        )}
         <button
           className='disk__create'
           onClick={() => {
