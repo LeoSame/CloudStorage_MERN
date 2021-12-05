b862aa864f0f770f632533912d177b5a64733c42
Leonid Semeniuk <leo.sam@ukr.net>
2021-12-05 12:33:17 +0200

    [FE]:ADD: Progress upload files

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,13 +1,14 @@
-import {applyMiddleware, combineReducers, createStore} from "redux";
-import {composeWithDevTools } from 'redux-devtools-extension'
-import thunk from "redux-thunk";
-import userReducer from "./userReducer";
-import fileReducer from "./fileReducer";
-
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import userReducer from './userReducer';
+import fileReducer from './fileReducer';
+import uploadReducer from './uploadReducer';
 
 const rootReducer = combineReducers({
-    user: userReducer,
-    files: fileReducer,
-})
+  user: userReducer,
+  files: fileReducer,
+  upload: uploadReducer,
+});
 
-export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+export const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
