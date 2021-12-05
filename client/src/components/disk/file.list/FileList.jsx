f41693a6cea08fadd2006877dbf89043d2abd56d
Leonid Semeniuk <leo.sam@ukr.net>
2021-12-05 13:32:12 +0200

    [FE]:ADD: Animations and formatting

--- client/src/components/disk/file.list/FileList.jsx
+++ client/src/components/disk/file.list/FileList.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import './fileList.css';
 import { useSelector } from 'react-redux';
 import File from './file/File';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 const FileList = () => {
   const files = useSelector(state => state.files.files);
-  const fileList = files.map(file => <File key={file._id} file={file} />);
+
+  const fileList = files.map(file => (
+    <CSSTransition key={file._id} timeout={500} classNames={'file'} exit={false}>
+      <File file={file} />
+    </CSSTransition>
+  ));
 
   return (
     <div className='filelist'>
@@ -14,7 +20,7 @@ const FileList = () => {
         <div className='filelist__date'>Дата</div>
         <div className='filelist__size'>Размер</div>
       </div>
-      {fileList}
+      <TransitionGroup>{fileList}</TransitionGroup>
     </div>
   );
 };
